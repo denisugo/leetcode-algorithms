58acb5837c9fdbc1265272e1ec44411a2adc48df
Denis Ustiuzhanin <superden1320@gmail.com>
2024-04-27 19:58:12 +0200

    Time: 391 ms (7.01%), Space: 22 MB (23.91%) - LeetHub

---
+++ 0937-online-stock-span/0937-online-stock-span.py
@@ -0,0 +1,14 @@
+class StockSpanner:
+
+    def __init__(self):
+        self.stack = []  # price, day_count
+
+    def next(self, price: int) -> int:
+        day_count = 1
+        while self.stack and not price < self.stack[-1][0]:
+            print(price, self.stack[-1])
+            day_count += self.stack.pop()[1]
+
+        self.stack.append((price, day_count))
+
+        return day_count
